8d83d352a72e4cf07d4c5f8124c14540ae7b6a1f
Karl Eduard Vaina <103428011+karlviana@users.noreply.github.com>
2023-11-15 14:26:37 +0800

    changes

---
+++ plants/src/components/Navbar.js
@@ -0,0 +1,18 @@
+
+const Navbar = () => {
+   return(
+    <nav className='w-full h-[70px] flex justify-between items-center font-bold bg-[#F4E6D5] absolute'>
+            <div className='bg-[#EED5B9] relative w-[200px]'>
+                <h1>plada</h1>
+            </div>
+
+           <ul className=''>
+            <li>Home</li>
+            <li>Products</li>
+            <li></li>
+           </ul>
+    </nav>
+   )
+}
+
+export default Navbar
