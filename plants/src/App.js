e721cef11214b165cce694d1f7a86bf1228d1f67
Karl Eduard Vaina <103428011+karlviana@users.noreply.github.com>
2023-11-17 14:55:58 +0800

    sa

--- plants/src/App.js
+++ plants/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Ourproduct from './components/Ourproduct';
 
 function App() {
   return (
     <>
     <Navbar />
     <Home />
+    <Ourproduct />
     </>
   );
 }
---
+++ plants/src/components/Ourproduct.js
@@ -0,0 +1,24 @@
+import '../css/Ourproduct.css';
+
+const Ourproduct = () => {
+    return(
+        <div name='ourproduct' className="w-full min-h-screen">
+         <div className='backg1 w-full grid grid-cols sm:grid-cols-3'>
+         <div>
+            <img alt='Product 1'/>
+         </div>
+         <div>
+            <img alt='Product 2'/>
+         </div>
+          <div>
+            <img alt='Product 3'/>
+         </div>
+         </div>
+         <div className='backg2 w-full'>
+ 
+         </div>
+        </div>
+    )
+}
+
+export default Ourproduct
